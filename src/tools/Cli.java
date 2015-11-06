9739cc40239fffd8334eed9c6bf31044dd55b9f1
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2015-11-06 14:07:18 +0100

    parse command line arguments with apache commons cli library

--- src/tools/Cli.java
+++ src/tools/Cli.java
@@ -1,7 +1,37 @@
 package tools;
 
+import org.apache.commons.cli.*;
+
 /**
  * Created by Jasmin on 06.11.2015.
  */
 public class Cli {
+    private String[] _args = null;
+    private Options options = new Options();
+
+    public Cli(String[] args){
+        _args = args;
+
+        options.addOption("source", true, "Path or name of the source file");
+        options.addOption("target", true, "Path or name of the target file");
+
+        options.addOption("aPush", false, "Select the mode PUSH for Excercise A");
+        options.addOption("aPull", false, "Select the mode PULL for Excercise A");
+        options.addOption("bPush", false, "Select the mode PUSH for Excercise B");
+        options.addOption("bPull", false, "Select the mode PULL for Excercise B");
+
+    }
+
+    public CommandLine parse(){
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, _args);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return cmd;
+    }
 }
