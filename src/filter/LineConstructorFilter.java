3339d240e941f25df2c08b9a3227f317ae491369
jpo2433 <jasmin.pogatschneg@students.fhv.at>
2015-11-07 21:03:55 +0100

    pull b

--- src/filter/LineConstructorFilter.java
+++ src/filter/LineConstructorFilter.java
@@ -6,6 +6,8 @@ import entities.WordSequence;
 import interfaces.Readable;
 import interfaces.Writeable;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.security.InvalidParameterException;
 
 /**
@@ -14,6 +16,7 @@ import java.security.InvalidParameterException;
 public class LineConstructorFilter<in, out> extends DataEnrichmentFilter<Word, WordSequence> {
     private int _lineNumber = 1;
     private int _wordsLimit = 10;
+    private Writer _writer;
 
     public int getWordsLimit() {
         return _wordsLimit;
@@ -39,6 +42,11 @@ public class LineConstructorFilter<in, out> extends DataEnrichmentFilter<Word, W
         _lineNumber = 1;
     }
 
+    public LineConstructorFilter(Writer writer, Readable<Word> tReadable) throws InvalidParameterException {
+        super(tReadable);
+        _writer = writer;
+    }
+
     @Override
     protected boolean fillEntity(Word nextVal, WordSequence entity) {
         if (nextVal != null && entity.getWords() != null && entity.getWords().size() < getWordsLimit()) {
@@ -47,6 +55,11 @@ public class LineConstructorFilter<in, out> extends DataEnrichmentFilter<Word, W
         }
         //entity.setLine(entity.getLine().replace('\r', ' '));
         entity.setLineNumber(_lineNumber++);
+        try {
+            _writer.write(entity.GetWordSequenceAsString() + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 
--- src/tools/Controller.java
+++ src/tools/Controller.java
@@ -4,6 +4,7 @@ import entities.Line;
 import entities.Word;
 import entities.WordSequence;
 import filter.*;
+import interfaces.IOable;
 import interfaces.Writeable;
 import pipes.Pipe;
 import pipes.SplitPipe;
@@ -178,6 +179,49 @@ public class Controller {
     }
 
     public void bPull() {
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        BufferedWriter bufferedWriter2 = null;
 
+        try {
+            bufferedReader = new BufferedReader(new FileReader(_sourceFile));
+            bufferedWriter = new BufferedWriter(new FileWriter(_targetFile));
+            bufferedWriter2 = new BufferedWriter(new FileWriter(_targetFile2));
+
+            boolean isPush = false;
+
+            Source<Character> source = new Source(bufferedReader);
+            WordConstructorFilter<Character, Word> wcf = new WordConstructorFilter<>(source);
+            Pipe<Word> pipe1 = new Pipe(wcf, isPush);
+            LineConstructorFilter<Word, WordSequence> lcf = new LineConstructorFilter<>(bufferedWriter2,pipe1);
+            lcf.setWordsLimit(_lineLength);
+            Pipe<WordSequence> pipe2 = new Pipe(lcf, isPush);
+            CirculationFilter<WordSequence, List<WordSequence>> cf = new CirculationFilter<>(pipe2);
+            Pipe<List<WordSequence>> pipe3 = new Pipe((interfaces.Readable) cf);
+            ABCOrderFilter_2<List<WordSequence>> abcFilter = new ABCOrderFilter_2((interfaces.Readable<List<WordSequence>>) pipe3);
+            Pipe<List<WordSequence>> pipe4 = new Pipe(abcFilter, isPush);
+            Sink<List<WordSequence>> sink = new Sink<>(bufferedWriter, pipe4);
+
+            Thread thread1 = new Thread(sink);
+            thread1.run();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
